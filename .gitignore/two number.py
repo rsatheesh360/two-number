8208b64560e5d1cf5e6ba0a7f0398d96c5bf2b43
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-02-28 12:01:51 +0530

    given two number

---
+++ .gitignore/two number.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the number:"))
+if(n<1 or (n%1)!=0):
+    print("wrong input")
+else:
+    temp=0
+    while(n!=0):
+        temp=temp+n
+        n=n-1
+    print (temp)
+    print (temp)
